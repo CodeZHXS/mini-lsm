20231b390564cf573012f9bc4fd8cbfb7e1d57ce
chenyujie_linux2 <chenyujie6691@qq.com>
2025-03-17 15:13:24 +0800

    pass week1 day3 task1

--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -12,10 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
+use bytes::BufMut;
 
-use crate::key::{KeySlice, KeyVec};
+use crate::{
+    block::SIZEOF_U16,
+    key::{KeySlice, KeyVec},
+};
 
 use super::Block;
 
@@ -34,22 +36,62 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: vec![],
+            data: vec![],
+            block_size,
+            first_key: KeyVec::new(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        assert!(!key.is_empty());
+        assert!(key.len() <= std::u16::MAX as usize);
+        assert!(value.len() <= std::u16::MAX as usize);
+
+        let key_len = key.len() as u16;
+        let value_len = value.len() as u16;
+
+        if self.is_empty() {
+            self.add_kv(key_len, key, value_len, value);
+            self.first_key = key.to_key_vec();
+            return true;
+        }
+
+        if self.current_block_size() + (key_len + value_len) as usize + SIZEOF_U16 * 3
+            > self.block_size
+        {
+            return false;
+        }
+
+        self.add_kv(key_len, key, value_len, value);
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.data.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
+    }
+
+    fn current_block_size(&self) -> usize {
+        self.data.len() + self.offsets.len()
+    }
+
+    fn add_kv(&mut self, key_len: u16, key: KeySlice, value_len: u16, value: &[u8]) {
+        self.offsets.push(self.data.len() as u16);
+        self.data.put_u16(key_len);
+        self.data.put(key.raw_ref());
+        self.data.put_u16(value_len);
+        self.data.put(value);
     }
 }
