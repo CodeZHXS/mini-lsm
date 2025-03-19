6edf6424b4057a7f1b1dd4e3c1a4a12e44308cae
chenyujie_linux2 <chenyujie6691@qq.com>
2025-03-19 15:58:40 +0800

    pass week1 day6 task1

--- mini-lsm-starter/src/lsm_storage.rs
+++ mini-lsm-starter/src/lsm_storage.rs
@@ -39,7 +39,7 @@ use crate::lsm_iterator::{FusedIterator, LsmIterator};
 use crate::manifest::Manifest;
 use crate::mem_table::MemTable;
 use crate::mvcc::LsmMvccInner;
-use crate::table::{SsTable, SsTableIterator};
+use crate::table::{SsTable, SsTableBuilder, SsTableIterator};
 
 pub type BlockCache = moka::sync::Cache<(usize, usize), Arc<Block>>;
 
@@ -360,7 +360,7 @@ impl LsmStorageInner {
         unimplemented!()
     }
 
-    fn memtable_capacity_limit(&self) -> usize {
+    fn target_sst_size(&self) -> usize {
         self.options.target_sst_size
     }
 
@@ -397,9 +397,9 @@ impl LsmStorageInner {
     }
 
     pub fn try_freeze_memtable(&self, new_approximate_size: usize) -> Result<()> {
-        if new_approximate_size >= self.memtable_capacity_limit() {
+        if new_approximate_size >= self.target_sst_size() {
             let state_lock = self.state_lock.lock();
-            if self.state.read().memtable.approximate_size() >= self.memtable_capacity_limit() {
+            if self.state.read().memtable.approximate_size() >= self.target_sst_size() {
                 self.force_freeze_memtable(&state_lock)?;
             }
         }
@@ -420,7 +420,25 @@ impl LsmStorageInner {
 
     /// Force flush the earliest-created immutable memtable to disk
     pub fn force_flush_next_imm_memtable(&self) -> Result<()> {
-        unimplemented!()
+        let last_memtable = self.state.read().imm_memtables.last().unwrap().clone();
+        let mut sst_builder = SsTableBuilder::new(self.options.block_size);
+        last_memtable.flush(&mut sst_builder)?;
+
+        let id = last_memtable.id();
+        let sst = Arc::new(sst_builder.build(
+            id,
+            Some(self.block_cache.clone()),
+            self.path_of_sst(id),
+        )?);
+
+        let mut guard = self.state.write();
+        let mut snapshot = guard.as_ref().clone();
+        snapshot.imm_memtables.pop().unwrap();
+        snapshot.l0_sstables.insert(0, id);
+        snapshot.sstables.insert(id, sst);
+        *guard = Arc::new(snapshot);
+
+        Ok(())
     }
 
     pub fn new_txn(&self) -> Result<()> {
