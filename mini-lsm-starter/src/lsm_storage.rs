f10ef27f1f84fea6f364cb0b4e47d9d92257109f
chenyujie_linux2 <chenyujie6691@qq.com>
2025-03-25 17:03:50 +0800

    impl week2 day7 task1

--- mini-lsm-starter/src/lsm_storage.rs
+++ mini-lsm-starter/src/lsm_storage.rs
@@ -497,8 +497,25 @@ impl LsmStorageInner {
     }
 
     /// Write a batch of data into the storage. Implement in week 2 day 7.
-    pub fn write_batch<T: AsRef<[u8]>>(&self, _batch: &[WriteBatchRecord<T>]) -> Result<()> {
-        unimplemented!()
+    pub fn write_batch<T: AsRef<[u8]>>(&self, batch: &[WriteBatchRecord<T>]) -> Result<()> {
+        for record in batch {
+            match record {
+                WriteBatchRecord::Put(key, value) => {
+                    let key = key.as_ref();
+                    let value = value.as_ref();
+                    let new_approximate_size =
+                        self.state.read().memtable.put_and_get_size(key, value);
+                    self.try_freeze_memtable(new_approximate_size)?
+                }
+                WriteBatchRecord::Del(key) => {
+                    let key = key.as_ref();
+                    let new_approximate_size =
+                        self.state.read().memtable.put_and_get_size(key, &[]);
+                    self.try_freeze_memtable(new_approximate_size)?;
+                }
+            }
+        }
+        Ok(())
     }
 
     fn target_sst_size(&self) -> usize {
@@ -507,14 +524,12 @@ impl LsmStorageInner {
 
     /// Put a key-value pair into the storage by writing into the current memtable.
     pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
-        let new_approximate_size = self.state.read().memtable.put_and_get_size(key, value);
-        self.try_freeze_memtable(new_approximate_size)
+        self.write_batch(&[WriteBatchRecord::Put(key, value)])
     }
 
     /// Remove a key from the storage by writing an empty value.
     pub fn delete(&self, key: &[u8]) -> Result<()> {
-        let new_approximate_size = self.state.read().memtable.put_and_get_size(key, &[]);
-        self.try_freeze_memtable(new_approximate_size)
+        self.write_batch(&[WriteBatchRecord::Del(key)])
     }
 
     pub(crate) fn path_of_sst_static(path: impl AsRef<Path>, id: usize) -> PathBuf {
