b6e7c87518a544408d694c67c0c5841e5a412666
chenyujie_linux2 <chenyujie6691@qq.com>
2025-03-31 22:38:42 +0800

    fix warning

--- mini-lsm-starter/src/mem_table.rs
+++ mini-lsm-starter/src/mem_table.rs
@@ -148,7 +148,7 @@ impl MemTable {
     /// Get a value by key.
     pub fn get(&self, key: KeySlice) -> Option<Bytes> {
         let key_bytes = KeyBytes::from_bytes_with_ts(
-            Bytes::from_static(unsafe { std::mem::transmute(key.key_ref()) }),
+            Bytes::from_static(unsafe { std::mem::transmute::<&[u8], &[u8]>(key.key_ref()) }),
             key.ts(),
         );
         self.map.get(&key_bytes).map(|entry| entry.value().clone())
