95d6b818caa6205ebddc1ecee2bc2b52094615f1
chenyujie_linux2 <chenyujie6691@qq.com>
2025-04-03 10:48:32 +0800

    pass week3 day4 task2

--- mini-lsm-starter/src/mvcc.rs
+++ mini-lsm-starter/src/mvcc.rs
@@ -70,9 +70,11 @@ impl LsmMvccInner {
     }
 
     pub fn new_txn(&self, inner: Arc<LsmStorageInner>, serializable: bool) -> Arc<Transaction> {
-        let ts = self.ts.lock();
+        let mut ts = self.ts.lock();
+        let read_ts = ts.1.watermark().unwrap_or(ts.0);
+        ts.1.add_reader(read_ts);
         Arc::new(Transaction {
-            read_ts: ts.0,
+            read_ts: read_ts,
             inner,
             local_storage: Arc::new(SkipMap::new()),
             committed: Arc::new(AtomicBool::new(false)),
